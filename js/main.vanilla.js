57e6a7421306477161d4cc024da6c62e56a81b3f
Marshall Ku <marshall@kakao.com>
2020-06-29 23:18:46 +0900

    Update main.vanilla.js

--- js/main.vanilla.js
+++ js/main.vanilla.js
@@ -1,63 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const modalTrigger = document.querySelector(".cd-modal-trigger"),
-		transitionLayer = document.querySelector(".cd-transition-layer"),
-		transitionBackground = transitionLayer.querySelector(".bg-layer"),
-		modalWindow = document.querySelector(".cd-modal");
+    const modalTrigger = document.querySelector(".cd-modal-trigger"),
+        transitionLayer = document.querySelector(".cd-transition-layer"),
+        transitionBackground = transitionLayer.querySelector(".bg-layer"),
+        modalWindow = document.querySelector(".cd-modal");
 
-	const frameProportion = 1.70,
-		frames = 25;
+    function animationEndHandler() {
+        const animEnd = () => {
+            transitionLayer.classList.contains("closing") && (
+                transitionLayer.classList.remove("closing", "opening", "visible")
+            )
+        };
 
-	let resize;
+        transitionBackground.addEventListener("animationend", animEnd),
+        transitionBackground.addEventListener("webkitAnimationend", animEnd)
+    }
+    animationEndHandler(),
 
-	function setLayerDimensions() {
-		const windowWidth = window.innerWidth,
-			windowHeight = window.innerHeight,
-			condition = windowWidth / windowHeight > frameProportion;
-		let layerHeight, layerWidth;
+    modalTrigger.addEventListener("click", e => {
+        const delay = document.querySelector(".no-cssanimations") ? 0 : 600;
+        e.preventDefault(),
+        transitionLayer.classList.add("visible", "opening");
+        setTimeout(() => {
+            modalWindow.classList.add("visible")
+        }, delay)
+    }),
 
-		layerWidth = `${condition ? windowWidth : windowHeight*1.2*frameProportion}`,
-		layerHeight = `${condition ? layerWidth/frameProportion : windowHeight*1.2}`,
-
-		transitionBackground.style.width = `${layerWidth*frames}px`,
-		transitionBackground.style.height = `${layerHeight}px`,
-
-		resize = false;
-	}
-
-	function animationEndHandler() {
-		const animEnd = () => {
-			transitionLayer.classList.contains("closing") && (
-				transitionLayer.classList.remove("closing", "opening", "visible")
-			)
-		};
-
-		transitionBackground.addEventListener("animationend", animEnd),
-		transitionBackground.addEventListener("webkitAnimationend", animEnd)
-	}
-
-	setLayerDimensions(),
-	animationEndHandler(),
-
-	modalTrigger.addEventListener("click", e => {
-		const delay = document.querySelector(".no-cssanimations") ? 0 : 600;
-		e.preventDefault(),
-		transitionLayer.classList.add("visible", "opening");
-		setTimeout(() => {
-			modalWindow.classList.add("visible")
-		}, delay)
-	}),
-
-	modalWindow.querySelector(".modal-close").addEventListener("click", e => {
-		e.preventDefault(),
-		transitionLayer.classList.add("closing"),
-		modalWindow.classList.remove("visible")
-	}),
-
-	window.addEventListener("resize", () => {
-		resize || (
-			resize = true,
-			(!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions)
-		)
-	})
+    modalWindow.querySelector(".modal-close").addEventListener("click", e => {
+        e.preventDefault(),
+        transitionLayer.classList.add("closing"),
+        modalWindow.classList.remove("visible")
+    })
 
 })
